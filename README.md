f0d18fbae3550dcc590025c7064aa5c341651c21
Divya Kattegummula <dkattegummula@corp.logitech.com>
2019-08-20 13:08:23 -0700

    adding image

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cicd-buzz
 https://damp-sea-35545.herokuapp.com/
+
+https://medium.com/bettercode/how-to-build-a-modern-ci-cd-pipeline-5faa01891a5b
--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route("/")
 def generate_buzz():
     """generate flask wrapper"""
-    page = '<html><body><h1>'
+    page = '<html><body background="./images/calm-bg.jpg text-align:center" = ><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
